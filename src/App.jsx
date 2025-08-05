4bb914c5d8acd6e9b8d0d0c4013c0d5ec42d1cc8
VaydaantE <98404387+VaydaantE@users.noreply.github.com>
2025-08-04 22:06:49 -0500

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { HashRouter, Route, Routes } from 'react-router'
-
-import AboutUs from './components/AboutUs'
-import OtherInfo from './components/OtherInfo'
+import './App.css';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
-import CountdownTimer from './components/CountdownTimer';
+import Footer from './components/Footer';
+import Home from './components/Home';
 import SignupForm from './components/SignupForm';
 import DonationTracker from './components/DonationTracker';
 import EventInfo from './components/EventInfo';
 import FAQ from './components/FAQ';
 import EventHighlights from "./components/EventHighlights";
+import CommunityBoard from './components/CommunityBoard';
+import { Container } from 'react-bootstrap';
+import VolunteerPage from './components/VolunteerPage';
 
 function App() {
   return (
     <HashRouter>
       <Header />
-      <CountdownTimer />
-
-      <Routes>
-        <Route path="/" element={<AboutUs />} />
-        <Route path="/signup" element={<SignupForm />} />
-        <Route path="/donate" element={<DonationTracker />} />
-        <Route path="/event-info" element={<EventInfo />} />
-        <Route path="/faq" element={<FAQ />} />
-        <Route path="/highlights" element={<EventHighlights />} />
-      </Routes>
+      <Container fluid style={{ marginTop: '110px', minHeight: '80vh' }}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signup" element={<SignupForm />} />
+          <Route path="/donate" element={<DonationTracker />} />
+          <Route path="/event-info" element={<EventInfo />} />
+          <Route path="/faq" element={<FAQ />} />
+          <Route path="/highlights" element={<EventHighlights />} />
+          <Route path="/community" element={<CommunityBoard />} />
+          <Route path="/volunteer" element={<VolunteerPage />} />
+        </Routes>
+      </Container>
+      <Footer />
     </HashRouter>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import { UserProvider } from './components/UserContext.jsx';
 
 createRoot(document.getElementById('root')).render(
-  <App />
-)
+  <UserProvider> 
+    <App />
+  </UserProvider>
+);
